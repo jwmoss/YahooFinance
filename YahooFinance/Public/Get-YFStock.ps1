3f5cdbe9ed2a6aae16375ab234ae550a99dc48ba
Jonathan Moss <jwmoss88@gmail.com>
2020-08-02 15:36:31 -0400

    updates **NO_CI**

--- YahooFinance/Public/Get-YFStock.ps1
+++ YahooFinance/Public/Get-YFStock.ps1
@@ -4,11 +4,37 @@ function Get-YFStock {
         [string[]]
         $Ticker,
 
-        [ValidateSet( [PeriodGenerator] )]
+        [ValidateSet(
+            "1d",
+            "5d",
+            "1mo",
+            "3mo",
+            "6mo",
+            "1y",
+            "2y",
+            "5y",
+            "10y",
+            "ytd",
+            "max"
+        )]
         [string]
         $Period = "max",
 
-        [ValidateSet( [IntervalGenerator] )]
+        [ValidateSet(
+            "1m",
+            "2m",
+            "5m",
+            "15m",
+            "30m",
+            "60m",
+            "90m",
+            "1h",
+            "1d",
+            "5d",
+            "1wk",
+            "1mo",
+            "3mo"
+        )]
         [string]
         $Interval = "1d",
 
@@ -18,7 +44,10 @@ function Get-YFStock {
         [datetime]
         $End,
 
-        [ValidateSet('ticker', 'column')]
+        [ValidateSet(
+            'ticker',
+            'column'
+        )]
         [string]
         $GroupBy = "column",
 
