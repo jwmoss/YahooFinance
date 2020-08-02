f16c825cbc29b4b79e6f174eb1b8c5a19bc5d418
Jonathan Moss <jwmoss88@gmail.com>
2020-08-02 16:34:38 -0400

    More params

--- YahooFinance/Public/Get-YFStock.ps1
+++ YahooFinance/Public/Get-YFStock.ps1
@@ -2,73 +2,35 @@ function Get-YFStock {
     [CmdletBinding()]
     param(
         [string[]]
-        $Ticker,
-
-        [ValidateSet(
-            "1d",
-            "5d",
-            "1mo",
-            "3mo",
-            "6mo",
-            "1y",
-            "2y",
-            "5y",
-            "10y",
-            "ytd",
-            "max"
-        )]
-        [string]
-        $Period = "max",
-
-        [ValidateSet(
-            "1m",
-            "2m",
-            "5m",
-            "15m",
-            "30m",
-            "60m",
-            "90m",
-            "1h",
-            "1d",
-            "5d",
-            "1wk",
-            "1mo",
-            "3mo"
-        )]
-        [string]
-        $Interval = "1d",
-
-        [datetime]
-        $Start,
-
-        [datetime]
-        $End,
-
-        [ValidateSet(
-            'ticker',
-            'column'
-        )]
-        [string]
-        $GroupBy = "column",
-
-        [switch]
-        $PrePost,
-
-        [switch]
-        $AutoAdjust,
-
-        [switch]
-        $Actions,
-
-        [switch]
-        $Rounding
+        $Ticker
     )
 
-    if ($Tickers -is [array]) {
+    $Hash = @{ }
 
+    if ($Ticker -is [array]) {
+        $ticker.ForEach( {
+                $url = ("$script:url_base/v7/finance/options/{0}") -f $_
+                $results = Invoke-RestMethod -Method "Get" -Uri $url
+
+                if ($null -ne $results.optionChain.Error) {
+                    Write-Warning "Error while retrieivng $_"
+                }
+                else {
+                    $hash.Add($_, $results.optionChain.result)
+                }
+            })
     }
     else {
+        $url = ("$script:url_base/v7/finance/options/{0}") -f $ticker
+        $results = Invoke-RestMethod -Method "Get" -Uri $url
 
+        if ($null -ne $results.optionChain.Error) {
+            Write-Warning "Error while retrieivng $ticker"
+        }
+        else {
+            $hash.Add($Ticker, $results.optionChain.result)
+            $hash
+        }
     }
-
+    $hash
 }
