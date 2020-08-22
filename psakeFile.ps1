c76642945e38fefe0b551b7386df35a9d135bb39
Jonathan Moss <jwmoss88@gmail.com>
2020-08-22 11:08:21 -0400

    Add deploy step

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -8,3 +8,34 @@ properties {
 task default -depends Test
 
 task Test -FromModule PowerShellBuild -Version '0.4.0'
+
+task PublishToPSGallery -Action {
+
+    ## https://github.com/KevinMarquette/PSGraph/blob/master/BuildTasks/PublishModule.Task.ps1
+
+    if ($ENV:BHBranchName -eq "master" -and
+        $ENV:BHCommitMessage -match "!deploy" -and
+        -not [string]::IsNullOrWhiteSpace($ENV:API)) {
+
+        $publishModuleSplat = @{
+            Path        = $outputModDir
+            NuGetApiKey = $env:API
+            Verbose     = $true
+            Force       = $true
+            ErrorAction = 'Stop'
+        }
+        "Files in module output:"
+        Get-ChildItem $outputModDir -Recurse -File | Select-Object -Expand FullName
+
+        "Publishing [$Destination] to [Powershell Gallery]"
+
+        Publish-Module @publishModuleSplat
+    }
+
+    else {
+        "Skipping deployment: To deploy, ensure that...`n" +
+        "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
+        "`t* The repository APIKey is defined in `$ENV:nugetapikey (Current: $(![string]::IsNullOrWhiteSpace($ENV:API)))) `n" +
+        "`t* This is not a pull request"
+    }
+}
